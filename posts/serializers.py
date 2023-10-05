0830517632179a6ca13fdbbae73ed1e4c2897074
SoroushGReza <mycodeschool22@outlook.com>
2023-10-05 16:30:00 +0000

    Added: image_fitler choice field ang image validation

--- posts/serializers.py
+++ posts/serializers.py
@@ -8,6 +8,20 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
+    def validate_image(self, value):
+        if file.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError(
+                'Image size larger than 2MB!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width larger than 4096px!'
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        return value
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -18,5 +32,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'is_owner', 'profiles_id',
             'updated_at', 'content', 'image',
-            'created_at', 'profile_image', 'title'
+            'created_at', 'profile_image', 'title', 'image_filter'
         ]
