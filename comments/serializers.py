a84a8e1cbc21ea0b081d1b9b34a383ab92a3219b
SoroushGReza <mycodeschool22@outlook.com>
2023-10-06 18:30:48 +0000

    CommentList and CommentDetail generic views

--- comments/serializers.py
+++ comments/serializers.py
@@ -3,8 +3,14 @@ from .models import Comment
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Comment model
+    Adds three extra fields when returning a list of Comment instances
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -12,12 +18,15 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        # fields = '__all__'   <--- To include ALL fields
         fields = [
-            'id', 'owner', 'created_at', 'is_owner', 'profile_id',
-            'updated_at', 'content', 'post', 'profile_image'
+            'id', 'owner', 'is_owner', 'profile_id', 'profile_image',
+            'post', 'created_at', 'updated_at', 'content'
         ]
 
 
 class CommentDetailSerializer(CommentSerializer):
+    """
+    Serializer for the Comment model used in Detail view
+    Post is a read only field so that we dont have to set it on each update
+    """
     post = serializers.ReadOnlyField(source='post.id')
