a9928408c9662da076fc5ad8ceaece5431d8cd3e
SoroushGReza <mycodeschool22@outlook.com>
2023-10-07 16:43:53 +0000

    Refactoring: posts and profile views.py Fix: Corrected followers/serializers.py

--- followers/serializers.py
+++ followers/serializers.py
@@ -4,18 +4,21 @@ from .models import Follower
 
 
 class FollowerSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Follower model
+    Create method handles the unique constraint on 'owner' and 'followed'
+    """
     owner = serializers.ReadOnlyField(source='owner.username')
-    is_owner = serializers.SerializerMethodField()
+    followed_name = serializers.ReadOnlyField(source='followed.username')
 
     class Meta:
         model = Follower
-        # fields = '__all__'   <--- To include ALL fields
         fields = [
             'id', 'owner', 'created_at', 'followed', 'followed_name'
         ]
-    
+
     def create(self, validated_data):
         try:
             return super().create(validated_data)
         except IntegrityError:
-            raise serializers.ValidationError({'detail': 'possible duplicates'})
+            raise serializers.ValidationError({'detail': 'possible duplicate'})
