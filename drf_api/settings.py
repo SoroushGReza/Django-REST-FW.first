db5bba4fe95cfd20562ca1e656dadd1e7efe4ac5
SoroushGReza <mycodeschool22@outlook.com>
2023-10-07 11:36:19 +0000

    Like Model Challenge:
    1. Created: 'likes' App.
    2. Implemented: 'Like' model.
    3. Runned: makemigrations and migrate

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -51,9 +51,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cloudinary',
     'rest_framework',
+
     'profiles',
     'posts',
     'comments',
+    'likes',
 ]
 
 MIDDLEWARE = [
