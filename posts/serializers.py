603332a223a27e7b12771b5147f7cb5bcc538c7c
SoroushGReza <mycodeschool22@outlook.com>
2023-10-09 16:41:48 +0000

    Adding extra fields to Post Challenge
    Added: comments_count and likes_count fields to the queryset

--- posts/serializers.py
+++ posts/serializers.py
@@ -9,6 +9,8 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -44,5 +46,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'updated_at', 'content', 'image',
             'created_at', 'profile_image', 'title', 'image_filter',
-            'like_id',
+            'like_id', 'likes_count', 'comments_count',
         ]
