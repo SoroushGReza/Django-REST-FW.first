7e9e6c295e9ed29e81874cdb00b273ed9589409b
SoroushGReza <mycodeschool22@outlook.com>
2023-10-10 10:17:13 +0000

    Add the filter feauture to our API Lesson
    Added: post and profile filters

--- profiles/views.py
+++ profiles/views.py
@@ -64,6 +64,7 @@
 # This code is refactored 
 from django.db.models import Count
 from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -81,7 +82,11 @@ class ProfileList(generics.ListAPIView):
     ).order_by('-created_at')
     serializer_class = ProfileSerializer
     filter_backends = [
-        filters.OrderingFilter
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filerset_fields = [
+        'owner__following__followed__profile',
     ]
     ordering_fields = [
         'posts_count',
