ea614b433f58438403d27797fbf780d8cce54b46
SoroushGReza <mycodeschool22@outlook.com>
2023-10-10 15:14:51 +0000

    Fix: Corrected syntax in settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -58,7 +58,7 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in on.environ
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-soroushgrez-djangorestf-2qrxvtkn8lg.ws-us105.gitpod.io',
