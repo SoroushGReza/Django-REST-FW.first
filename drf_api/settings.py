259aec90936f9dd9bab43bce36513e16b60e3b31
SoroushGReza <mycodeschool22@outlook.com>
2023-10-12 16:19:55 +0000

    Combined two projects

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import re
 
 if os.path.exists('env.py'):
     import env
@@ -59,13 +58,14 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+DEBUG = 'DEBUG' in os.environ
 
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
     '8000-soroushgrez-djangorestf-2qrxvtkn8lg.ws-us105.gitpod.io',
     'localhost',
+    '3000-soroushgrez-djangorestf-2qrxvtkn8lg.ws-us105.gitpod.io',
 ]
 
 
@@ -108,10 +108,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
     ]
 
 CORS_ALLOWED_CREDENTIALS = True
---
+++ frontend/src/api/axiosDefault.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+// axios.defaults.baseURL = 'https://django-rest-framwork-first-3168ada70b98.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true;
+
+export const axiosRec = axios.create();
+export const axiosRes = axios.create();
