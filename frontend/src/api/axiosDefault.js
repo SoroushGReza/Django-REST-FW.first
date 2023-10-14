a66bb6f99612b888ff705ac1fef85e9d04a23ebb
SoroushGReza <mycodeschool22@outlook.com>
2023-10-14 13:37:44 +0000

    Removed: frondend to clone again

--- frontend/src/api/axiosDefault.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// axios.defaults.baseURL = 'https://django-rest-framwork-first-3168ada70b98.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true;
-
-export const axiosRec = axios.create();
-export const axiosRes = axios.create();
