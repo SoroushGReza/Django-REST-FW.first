ea97f7157f19979cacbe0a91343ff8c65f723b18
SoroushGReza <mycodeschool22@outlook.com>
2023-10-18 10:01:17 +0000

    Liking and Unliking posta

--- frontend/src/pages/posts/Posts.js
+++ frontend/src/pages/posts/Posts.js
@@ -4,6 +4,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Post = (props) => {
   const {
@@ -19,10 +20,43 @@ const Post = (props) => {
     image,
     updated_at,
     postPage,
+    setPosts,
   } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post: id });
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_count: post.likes_count + 1, like_id: data.id }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleUnlike = async () => {
+    try {
+      await axiosRes.delete(`/likes/${like_id}/`);
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? { ...post, likes_count: post.likes_count - 1, like_id: null }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Card className={styles.Post}>
@@ -32,7 +66,7 @@ const Post = (props) => {
             <Avatar src={profile_image} height={55} />
             {owner}
           </Link>
-          <div>
+          <div className="d-flex align-items-center">
             <span>{updated_at}</span>
             {is_owner && postPage && "..."}
           </div>
@@ -45,28 +79,34 @@ const Post = (props) => {
         {title && <Card.Title className="text-center">{title}</Card.Title>}
         {content && <Card.Text>{content}</Card.Text>}
         <div className={styles.PostBar}>
-            {is_owner ? (
-                <OverlayTrigger placement="top" overlay={<Tooltip>You can't like your own post!</Tooltip>}>
-                    <i className="far fa-heart" />
-                </OverlayTrigger>
-            ) : like_id ? (
-                <span onClick={()=>{}}>
-                    <i className={`fas fa-heart ${styles.Heart}`} />
-                </span>
-            ) : currentUser ? (
-                <span onClick={() => {}}>
-                    <i className={`far fa-heart ${styles.HeartOutline}`} />
-                </span>
-            ) : (
-                <OverlayTrigger placement="top" overlay={<Tooltip>Log in to like posts!</Tooltip>}>
-                    <i className="far fa-heart" />
-                </OverlayTrigger>
-            )}
-            {likes_count}
-            <Link>
-              <i className="far fa-comments" />
-            </Link>
-            {comments_count}
+          {is_owner ? (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>You can't like your own post!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          ) : like_id ? (
+            <span onClick={handleUnlike}>
+              <i className={`fas fa-heart ${styles.Heart}`} />
+            </span>
+          ) : currentUser ? (
+            <span onClick={handleLike}>
+              <i className={`far fa-heart ${styles.HeartOutline}`} />
+            </span>
+          ) : (
+            <OverlayTrigger
+              placement="top"
+              overlay={<Tooltip>Log in to like posts!</Tooltip>}
+            >
+              <i className="far fa-heart" />
+            </OverlayTrigger>
+          )}
+          {likes_count}
+          <Link to={`/posts/${id}`}>
+            <i className="far fa-comments" />
+          </Link>
+          {comments_count}
         </div>
       </Card.Body>
     </Card>
